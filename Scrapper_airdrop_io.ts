4b196feab3fbbf50211d4e523cd675585faed67a
Nijeesh Joshy <nijeesh4all@gmail.com>
2018-04-08 04:03:35 +0530

    kk

--- Scrapper_airdrop_io.ts
+++ Scrapper_airdrop_io.ts
@@ -16,7 +16,7 @@ export default class Scrapper_airdrop_io {
                     let airdrop_object = {};
                     const regEx = new RegExp(/&apos[\S]{1,}&apos/g) 
                     
-                    let reg_url = $(element).parent().html();
+                    let reg_url:any = $(element).parent().html()+"";
                     
                     reg_url = regEx.exec(reg_url);
                     if(reg_url) {
