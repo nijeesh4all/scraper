65bb0bf7124e44c16dcfbca3217ff4af7a0f66ea
Nijeesh Joshy <nijeesh4all@gmail.com>
2018-04-11 01:04:16 +0530

    asd

--- Scrapper_airdrop_io.ts
+++ Scrapper_airdrop_io.ts
@@ -73,7 +73,7 @@ export default class Scrapper_airdrop_io {
                             // console.log(description_text);
                             // console.log(reg_url);
 
-                            const ico_info = dec_page.find('.airdrop-list > ul').html();
+                            const ico_info = dec_page.find('.airdrop-list').first().html();
                             console.log(ico_info);
                             console.log('-------')
                         }
