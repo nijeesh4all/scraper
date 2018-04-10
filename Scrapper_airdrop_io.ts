3872c61400fd9c01487e88437e4d7e41503f1a81
Nijeesh Joshy <nijeesh4all@gmail.com>
2018-04-11 01:30:07 +0530

    asdasd

--- Scrapper_airdrop_io.ts
+++ Scrapper_airdrop_io.ts
@@ -4,7 +4,7 @@ import * as request from 'request'
 
 export default class Scrapper_airdrop_io {
     private uri: string;
-    letructor(uri : string) {
+    constructor(uri : string) {
         this.uri = uri;
     }
 
@@ -15,7 +15,7 @@ export default class Scrapper_airdrop_io {
                 $('.inside-article').each((i,element) => {
                     let test :string;
                     let airdrop_object = {};
-                    let regEx = /&apos[\S]{1,}&apos/g; 
+                    const regEx = /&apos[\S]{1,}&apos/g; 
                     
                     let reg_url:any = $(element).parent().html().match(regEx);
                     if(reg_url){
@@ -24,16 +24,16 @@ export default class Scrapper_airdrop_io {
                     }else{
                         return null;
                     }
-                    let isExclusive = $(element).parent().attr('class').indexOf('exclusive')!= -1;
+                    const isExclusive = $(element).parent().attr('class').indexOf('exclusive')!= -1;
                     
-                    let airdrop = $(element).find('.air-wrapper').first();
-                    let img_url = airdrop.find('.air-thumbnail').first().find('img').attr('src');
-                    let content = $(airdrop.find('.air-content-front').first());
+                    const airdrop = $(element).find('.air-wrapper').first();
+                    const img_url = airdrop.find('.air-thumbnail').first().find('img').attr('src');
+                    const content = $(airdrop.find('.air-content-front').first());
                     
-                    let airdrop_name = content.find('a > h3').html()
-                    let airdrop_description_url = content.find('a > h3').attr('href');
-                    let airdrop_value  = content.find('.est-value').first().find('span').text()
-                    let airdrop_requirment = {};
+                    const airdrop_name = content.find('a > h3').html()
+                    const airdrop_description_url = content.find('a > h3').attr('href');
+                    const airdrop_value  = content.find('.est-value').first().find('span').text()
+                    const airdrop_requirment = {};
                     
                     content.find('ul.req-drop-list > li').each((i,req) => {
                          test = $(req).attr('title');
@@ -54,26 +54,26 @@ export default class Scrapper_airdrop_io {
 
                     request(reg_url,(dec_error,dec_response,dec_html) => {
                         if(!dec_error && dec_response.statusCode == 200){
-                            let  dec_page = $(dec_html).find('.inside-article').first();
+                            const  dec_page = $(dec_html).find('.inside-article').first();
                             
                             //info
-                            let info = $(dec_page).find('.airdrop-info > ul').children('li').first();
-                            let start_date = info.next().text().replace('Airdrop starts ','')
-                            let platform = info.next().next().next().text()
+                            const info = $(dec_page).find('.airdrop-info > ul').children('li').first();
+                            const start_date = info.next().text().replace('Airdrop starts ','')
+                            const platform = info.next().next().next().text()
                             //description
                             let description_text = $(dec_page).find('.drop-features > p').text();
                             description_text += "\n"+$(dec_page).find('.drop-features').next().text();
 
                             //Steps
 
-                            let steps_html = $(dec_page).find('.airdrop-guide').first().html();
+                            const steps_html = $(dec_page).find('.airdrop-guide').first().html();
 
                             //console.log(info);
                             // console.log(start_date);
                             // console.log(description_text);
                             // console.log(reg_url);
 
-                            let ico_info = $(dec_html).find('.airdrop-list>ul').find('li');
+                            const ico_info = $(dec_html).find('.airdrop-list>ul').find('li');
                             console.log(ico_info.length)
                             ico_info.each((ind,htm) => {
                                let li_element = $(htm);
